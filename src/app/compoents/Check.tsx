30f5d2b1bf02b90527e8e9983e34095e8f360d47
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-01-12 18:47:30 +0200

    Update Check.tsx

--- src/app/compoents/Check.tsx
+++ src/app/compoents/Check.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const Check = () => {
   return (
-    <div className='flex justify-center items-center space-x-5 p-5 bg-gray-100 rounded-lg bg-black'>
-      <div className='flex flex-col items-center'>
+    <div className='flex justify-center items-center space-x-5 p-5 rounded-lg bg-black'>
+      <div className='flex flex-col items-center bg-black'>
         <Image
           src='/android.png'
           width={500}
@@ -22,10 +22,13 @@ const Check = () => {
           width={500}
           height={500}
           alt='exp://u.expo.dev/update/bbbd645e-9326-4279-8792-87d9bd15fc3b'
-          className='border-2 border-gray-800 rounded-lg bg-black'
+          className='border-2 border-gray-800 rounded-lg'
         />
         <p className='mt-2 text-lg text-white-800'>Scan for iOS</p>
       </div>
+      <p className="text-center text-md text-white-700 mt-4">
+        Download the Expo app from the App Store or Google Play to get started.
+      </p>
     </div>
   )
 }
